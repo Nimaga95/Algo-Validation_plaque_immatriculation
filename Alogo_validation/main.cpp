081e3360aabf1b9e3122f8ac074d565b454929b5
Nimaga95 <117949986+Nimaga95@users.noreply.github.com>
2023-02-09 00:27:06 -0500

    Main

---
+++ Alogo_validation/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: elhadji.nimaga
+ *
+ * Created on 9 février 2023, 00 h 23
+ */
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
