c4caa6e1f76ef2c1ad820fc3050ce1a5f708fdb8
Gamani <117949986+Nimaga95@users.noreply.github.com>
2023-03-09 15:13:54 -0500

    Delete Alogo_validation/build/Debug/Cygwin-Windows directory

--- Alogo_validation/build/Debug/Cygwin-Windows/main.o.d
+++
@@ -1,2 +0,0 @@
-build/Debug/Cygwin-Windows/main.o: main.cpp validationimmatriculation.h
-validationimmatriculation.h:
--- Alogo_validation/build/Debug/Cygwin-Windows/validationimmatriculation.o.d
+++
@@ -1,2 +0,0 @@
-build/Debug/Cygwin-Windows/validationimmatriculation.o: \
- validationimmatriculation.cpp
